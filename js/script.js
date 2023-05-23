f1802a806052b2829f4003aa6b503e78ad828224
Raifurrahim <raifurrahimcse13@gmail.com>
2023-05-23 13:21:56 +0600

    frontend done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let navbar = document.querySelector('.navbar');
+document.querySelectorAll('#close').onclick  = () =>{
+    navbar.classList.remove('active');
+}
+var swiper = new Swiper(".product-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 100){
+        document.querySelector('header').classList.add('active');
+    }else{
+        document.querySelector('header').classList.remove('active');
+    }
+}
