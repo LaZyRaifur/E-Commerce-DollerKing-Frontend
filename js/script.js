39f234b8d43000250a95025c840ff6b5b787c234
Raifurrahim <raifurrahimcse13@gmail.com>
2023-05-23 16:28:45 +0600

    did some js but not accurate

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 let navbar = document.querySelector('.navbar');
+
+
+document.querySelector('#menu-bar').onclick = () =>{
+  navbar.classList.toggle('active');
+}
 document.querySelectorAll('#close').onclick  = () =>{
     navbar.classList.remove('active');
 }
+
+
+
 var swiper = new Swiper(".product-slider", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -27,4 +35,16 @@ window.onscroll = () =>{
     }else{
         document.querySelector('header').classList.remove('active');
     }
+}
+
+let themeToggler = document.querySelector ('#theme-toggler');
+themeToggler.onclick = () => {
+  themeToggler.classList.toggle('fa-sun');
+  if(themeToggler.classList.contains('fa-sun')){
+    document.querySelector ('body').classList.add('active');
+    
+  }else{
+    document.querySelector('body').classList.remove('active');
+  }
+
 }
